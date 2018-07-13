67b94c7d6de0783d7d7e3abeb7e79f5cd72793d3
SophieJanson <sophie@sophiejanson.com>
2018-07-13 08:34:29 +0200

    changed title and name

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
-export const baseUrl = 'https://still-chamber-38048.herokuapp.com'
+export const baseUrl = 'http://localhost:4000' //'https://still-chamber-38048.herokuapp.com'
 
 export const localStorageJwtKey = 'currentUserJwt'
